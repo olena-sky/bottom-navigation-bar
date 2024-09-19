f8b056cfbae34b6fcebbab806230cb60fb0ba5c1
Olena Stadnyk <olena.lightness@gmail.com>
2024-09-19 16:53:15 +0300

    feat: add expandable menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 /*=============== EXPANDED LIST ===============*/
+const navExpand = document.getElementById('nav-expand'),
+  navExpendList = document.getElementById('nav-expand-list'),
+  navExpendIcon = document.getElementById('nav-expand-icon')
 
+navExpand.addEventListener('click', () => {
+  // Expand list 
+  navExpendList.classList.toggle('show-list')
+
+  // Rotate icon 
+  navExpendIcon.classList.toggle('rotate-icon')
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
